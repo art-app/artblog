c8bd9a1cb64e4befa30bbb0c9724491173ee9f07
art-app <github@dimail.ru>
2023-06-15 20:26:33 +0300

    add route config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 import './styles/index.scss'
 import {classNames} from "shared/lib/classNames/lib/classNames";
 import {useTheme} from "app/providers/ThemeProvider/lib/useTheme";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 const App = () => {
 
@@ -17,12 +16,7 @@ const App = () => {
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
 
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />} />
-                    <Route path={'/'} element={<MainPage />} />
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
