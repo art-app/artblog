964221bfab8972bf7470718875564ef19b13e354
art-app <github@dimail.ru>
2023-06-15 06:05:30 +0300

    architecture refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
-import MainPageAsync from "./pages/MainPage/MainPage.async";
-import AboutPageAsync from "./pages/AboutPage/AboutPage.async";
+
 import './styles/index.scss'
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./shared/classNames/classNames";
+import {classNames} from "shared/lib/classNames/lib/classNames";
+import {useTheme} from "app/providers/ThemeProvider/lib/useTheme";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 const App = () => {
 
@@ -18,8 +19,8 @@ const App = () => {
 
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />} />
-                    <Route path={'/'} element={<MainPageAsync />} />
+                    <Route path={'/about'} element={<AboutPage />} />
+                    <Route path={'/'} element={<MainPage />} />
                 </Routes>
             </Suspense>
         </div>
