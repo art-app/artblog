8bfe1ddcb60045610082fd7139e2ddcbb9261cc0
--global <github@dimail.ru>
2023-09-13 21:30:02 +0300

    add husky

--- src/shared/ui/Input/Input.test.tsx
+++ src/shared/ui/Input/Input.test.tsx
@@ -1,15 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import React from 'react';
-import { Input } from 'shared/ui/Input/Input';
+import { Input } from './Input';
 
 describe('Input', () => {
-    test('test Input', () => {
-        render(<Input>test</Input>);
-        expect(screen.getByText('test')).toBeInTheDocument();
+    test('renders without crashing', () => {
+        const { container } = render(<Input />);
+        expect(container).toBeInTheDocument();
     });
 
-    test('test Input', () => {
-        render(<Input>test</Input>);
-        expect(screen.getByText('test')).toHaveClass('default');
+    test('renders a label if provided', () => {
+        const { getByText } = render(<Input label="Test Label" />);
+        const labelElement = getByText('Test Label');
+        expect(labelElement).toBeInTheDocument();
+    });
+
+    test('renders an input field with the correct type', () => {
+        const { container } = render(<Input type="password" />);
+        const inputElement = container.querySelector('input');
+        expect(inputElement).toHaveAttribute('type', 'password');
     });
 });
